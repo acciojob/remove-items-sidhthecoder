6da743ac81e92c88d1715ed6db29ffca8a7cd53d
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-19 14:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function Remove() {
+	var selectElement = document.getElementById("colorSelect");
+    var selectedValue = selectElement.value;
+    var selectedOption = selectElement.querySelector(`option[value="${selectedValue}"]`);
+    selectElement.removeChild(selectedOption);
+}
